f0cac52cc1a6fc414b59e803e43fe83eb1e26b1e
LIGU <guang.li@student.uclouvain.be>
2020-12-22 20:40:15 +0100

    as
    Commi

--- commands/aup.py
+++ commands/aup.py
@@ -1,28 +1,30 @@
 from commands.base_command import BaseCommand
-from time import *
+from asyncio import sleep
+from discord import User
 
 # Your friendly example event
 # Keep in mind that the command name will be derived from the class name
 # but in lowercase
 
 # So, a command class named Random will generate a 'random' command
-class fu(BaseCommand):
+class aup(BaseCommand):
 
     def __init__(self):
         # A quick description for the help message
-        description = "Insulte Hugo"
+        description = "Fais venir @User au pièd"
         # A list of parameters that the command will take as input
         # Parameters will be separated by spaces and fed to the 'params'
         # argument in the handle() method
         # If no params are expected, leave this list empty or set it to None
-        params = []
+        params = ["toutou"]
         super().__init__(description, params)
 
     # Override the handle() method
     # It will be called every time the command is received
     async def handle(self, params, message, client):
-        await message.channel.send("Fuck Hugo " + "<:Vibrator:689891174886146069>")
-        for i in range(3):
-            sleep(2)
-            await message.channel.send('Viens ici %s et fais waf %s ' % ('<@284414958932852736>',
+        toutou = params[0]
+        await message.channel.send("Au pièd <:bontoutou:790992228301668352>")
+        await sleep(3)
+        for i in range(5):
+            await message.channel.send('Viens ici %s et fais waf %s ' % (toutou,
                                                                      "<:bontoutou:790992228301668352>"))
--- commands/gamble.py
+++ commands/gamble.py
@@ -7,28 +7,27 @@ from random import randint
 # Keep in mind that the command name will be derived from the class name
 # but in lowercase
 # So, a command class named Random will generate a 'random' command
-class Random(BaseCommand):
+class gamble(BaseCommand):
 
     def __init__(self):
         # A quick description for the help message
-        description = "Donne un nombre aléatoire entre 1 et 100, les bornes peuvent etre spécifiées en argument"
+        description = "Donne un nombre aléatoire entre 1 et bound"
         # A list of parameters that the command will take as input
-        # Parameters will be separated by spaces and fed to the 'params' 
+        # Parameters will be separated by spaces and fed to the 'params'
         # argument in the handle() method
         # If no params are expected, leave this list empty or set it to None
-        params = []
+        params = ["adversaire","borne"]
         super().__init__(description, params)
 
     # Override the handle() method
     # It will be called every time the command is received
     async def handle(self, params, message, client):
-        # 'params' is a list that contains the parameters that the command 
+        # 'params' is a list that contains the parameters that the command
         # expects to receive, t is guaranteed to have AT LEAST as many
         # parameters as specified in __init__
         # 'message' is the discord.py Message object for the command to handle
         # 'client' is the bot Client object
         lower = 1
-        upper = 100
         if len(params) > 1:
             try:
                 lower = int(params[0])
