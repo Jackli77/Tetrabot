d43e9bd7345e49311b8d43228222a25273da2f5a
LIGU <guang.li@student.uclouvain.be>
2020-12-24 16:59:26 +0100

    Update random.py

--- commands/random.py
+++ commands/random.py
@@ -1,3 +1,5 @@
+from discord import Client
+
 from commands.base_command import BaseCommand
 from numpy.random import randint
 
@@ -45,8 +47,9 @@ class Random(BaseCommand):
             await message.channel.send(
                 "{0}, The lower bound can't be higher than the upper bound!".format(message.author.mention))
             return
-
+        aut_id = int(''.join(filter(str.isdigit, message.author.mention)))
+        aut_usr = await Client.fetch_user(client, aut_id)
         rolled = randint(lower, upper+1)
-        msg = "<:game_die:791035424507691013> {0}. La roulette va de **{1}** à **{2}** <:game_die:791035424507691013>".format(message.author.mention, lower, upper) + "\n" \
-              + "<:game_die:791035424507691013> La mère d'Hugo vient de tirer un **{0}**! pour {1} <:game_die:791035424507691013>".format(rolled,message.author.mention)
+        msg = "<:game_die:791035424507691013> **{0}**. La roulette va de **{1}** à **{2}** <:game_die:791035424507691013>".format(aut_usr.name, lower, upper) + "\n" \
+              + "<:game_die:791035424507691013> La mère d'Hugo vient de tirer un **{0}**! **{1}**<:game_die:791035424507691013>".format(rolled,aut_usr.display_name)
         await message.channel.send(msg)
