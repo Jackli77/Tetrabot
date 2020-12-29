03c1b661672a46967a43fbce55fff8a93e42a44b
LIGU <guang.li@student.uclouvain.be>
2020-12-29 22:40:03 +0100

    Postgre

--- database_init.py
+++ database_init.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 #Connect to DB
-DATABASE_URL = "postgres://eccexmdcwmfbfs:ffc3c2b5fdee5036a8ccc050911f405773c568ec757870655c1142e9f3ace6c6@ec2-3-248-4-172.eu-west-1.compute.amazonaws.com:5432/d2tvutmq0s0kd0"
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def initialize():
