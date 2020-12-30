01b7f14f6276ef33f58cc1dbf1d5cd5b613e971e
LIGU <guang.li@student.uclouvain.be>
2020-12-30 12:22:20 +0100

    Equity

--- commands/exo.py
+++ commands/exo.py
@@ -1,25 +1,13 @@
 import random
 from commands.base_command import BaseCommand
 
-# Your friendly example event
-# Keep in mind that the command name will be derived from the class name
-# but in lowercase
-
-# So, a command class named Random will generate a 'random' command
 class exo(BaseCommand):
 
     def __init__(self):
-        # A quick description for the help message
         description = "Calcule ton bénéf sur un exo"
-        # A list of parameters that the command will take as input
-        # Parameters will be separated by spaces and fed to the 'params'
-        # argument in the handle() method
-        # If no params are expected, leave this list empty or set it to None
         params = []
         super().__init__(description, params)
 
-    # Override the handle() method
-    # It will be called every time the command is received
     async def handle(self, params, message, client):
         prix = 30000
         if(len(params) > 0):
